b5183d74031f41b2e6d5bd0aa588ae93627217e2
cassandrahirata <cassandrahirata@gmail.com>
2023-04-10 21:38:53 -0700

    update

--- script.js
+++
@@ -1,6 +0,0 @@
-const selectProject = document.querySelector(".prevProjects");
-selectProject.addEventListener("change", (e) => {
-    const display = document.querySelector(".projectDisplay");
-    display.textContent = `${e.target.value}`;
-
-})
